246daf11a4b04cd034e188aa2319c907900f92cb
shrikant-boddul <61618073+shrikant-boddul@users.noreply.github.com>
2026-01-19 13:33:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "one" {
   count                  = 5
   ami                    = "ami-07ff62358b87c7116"
-  instance_type          = "c7i-flex.large"
+  instance_type          = "t2.medium"
   key_name               = "ubuntu-us-east-1"
   vpc_security_group_ids = ["sg-046cd16cdb7a78595"]
   tags = {
